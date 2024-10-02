671cd65257226c86d7034dae75aa01936f286caa
Robin31 <robindenis@hotmail.fr>
2024-10-02 22:56:54 +0200

    quete06 complitied

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { ArticleComponentComponent } from './article-component/article-component.component';
 import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
 import { ArticleSoloComponent } from './article-solo/article-solo.component';
+import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [
   {path: '', component: ArticleComponentComponent},
   {path: 'article/:id', component: ArticleSoloComponent},
+  {path: 'contact', component: ContactComponent},
   {path: '**', component: NotFoundPageComponent},
 ];
